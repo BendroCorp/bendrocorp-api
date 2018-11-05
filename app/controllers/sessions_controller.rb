6076a04f97557c8f533cd3f5923809f16f5138f5
Dale Myszewski II <dale.myszewski@seagate.com>
2018-11-04 23:53:57 -0600

    Additional site logging around auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,47 +7,55 @@ class SessionsController < ApplicationController
      @user = User.find_by email: params[:session][:email]
      # Does the user exist?
      if @user != nil
-       if @user.authenticate(params[:session][:password]) && (!@user.use_two_factor || (@user.use_two_factor && @user.two_factor_valid(params[:session][:code]))) && (!@user.locked && @user.login_allowed && @user.active)
-         # || (@user.use_two_factor && @user.two_factor_valid(params[:session][:code]))) && (!@user.locked && @user.login_allowed)
-         # puts "User is authorized: #{@user && !@user.active && @user.authenticate(params[:session][:password]) && (!@user.use_two_factor || (@user.use_two_factor && @user.two_factor_valid(params[:session][:code]))) && (!@user.locked && @user.login_allowed)}"
-         # puts "Creating token"
-         token_text = make_token
-         # token is not perpetual
-         new_token = UserToken.new(token: token_text, device: params[:session][:device], expires: Time.now + 12.hours) if !params[:session][:perpetual]
-         # token is perpetual
-         new_token = UserToken.new(token: token_text, device: params[:session][:device]) if params[:session][:perpetual]
-         @user.user_tokens << new_token
-         @user.login_attempts = 0
-         if @user.save
-           puts
-           puts "User authenticated and token created!"
-           puts
-           render status: 200, json: { id: @user.id, character: @user.main_character.as_json(only: [:id, :first_name, :last_name], methods: [:full_name, :avatar_url, :current_job]), tfa_enabled: @user.use_two_factor, token: token_text, token_expires: new_token.expires_ms, claims: @user.claims }
-         else
-           render status: 500, json: { message: "Login was not successful because: #{@user.errors.full_messages.to_sentence}" }
-         end
-       # if the auth fails
-       else
-         # if the account is not already locked
-         if !@user.locked
-           @user.login_attempts += 1
-           # if threshold exceeded then lock the account
-           if @user.login_attempts >= 5
-             @user.locked = true
-           end
+       if !@user.locked && @user.login_allowed && @user.active
+         if @user.authenticate(params[:session][:password]) && (!@user.use_two_factor || (@user.use_two_factor && @user.two_factor_valid(params[:session][:code])))
+           # || (@user.use_two_factor && @user.two_factor_valid(params[:session][:code]))) && (!@user.locked && @user.login_allowed)
+           # puts "User is authorized: #{@user && !@user.active && @user.authenticate(params[:session][:password]) && (!@user.use_two_factor || (@user.use_two_factor && @user.two_factor_valid(params[:session][:code]))) && (!@user.locked && @user.login_allowed)}"
+           # puts "Creating token"
+           token_text = make_token
+           # token is not perpetual
+           new_token = UserToken.new(token: token_text, device: params[:session][:device], expires: Time.now + 12.hours) if !params[:session][:perpetual]
+           # token is perpetual
+           new_token = UserToken.new(token: token_text, device: params[:session][:device]) if params[:session][:perpetual]
+           @user.user_tokens << new_token
+           @user.login_attempts = 0
            if @user.save
-             # if we locked the account then notify the user
-             if @user.locked
-               # TODO: email the user
-               # send_email @user.email, "Account Lockout", "<div><p><h3>Account Lockout</h3></p><p>It looks like there is a security issue with your account. Please reply to this email to start the unlock process.</p></div>"
-             end
+             SiteLog.create(module: 'Session', submodule: 'Success', message: "User ##{@user.id} successfully authenticated!", site_log_type_id: 1)
+             puts
+             puts "User authenticated and token created!"
+             puts
+             render status: 200, json: { id: @user.id, character: @user.main_character.as_json(only: [:id, :first_name, :last_name], methods: [:full_name, :avatar_url, :current_job]), tfa_enabled: @user.use_two_factor, token: token_text, token_expires: new_token.expires_ms, claims: @user.claims }
            else
-             render status: 500, json: { message: "Error occured while trying to access user data."}
+             render status: 500, json: { message: "Login was not successful because: #{@user.errors.full_messages.to_sentence}" }
            end
+         # if the auth fails
+         else
+           # if the account is not already locked
+           if !@user.locked
+             @user.login_attempts += 1
+             # if threshold exceeded then lock the account
+             if @user.login_attempts >= 5
+               @user.locked = true
+             end
+             if @user.save
+               # if we locked the account then notify the user
+               if @user.locked
+                 # TODO: email the user
+                 # send_email @user.email, "Account Lockout", "<div><p><h3>Account Lockout</h3></p><p>It looks like there is a security issue with your account. Please reply to this email to start the unlock process.</p></div>"
+               end
+             else
+               render status: 500, json: { message: "Error occured while trying to access user data."}
+             end
+           end
+           SiteLog.create(module: 'Session', submodule: 'Auth Failure', message: "User ##{@user.id} could not authenticated. Authorization failed.", site_log_type_id: 1)
+           render status: 403, json: { message: "User not found or incorrect credentials were provided." }
          end
-         render status: 403, json: { message: "User not found or incorrect credentials were provided." }
+       else
+         SiteLog.create(module: 'Session', submodule: 'Account Locked', message: "User ##{@user.id} could not authenticated. Account is locked.", site_log_type_id: 1)
+         render status: 403, json: { message: 'User not found or incorrect credentials were provided.' }
        end
      else
+       SiteLog.create(module: 'Session', submodule: 'User does not exist', message: "User could not authenticated. User does not exist!", site_log_type_id: 1)
        render status: 403, json: { message: 'User not found or incorrect credentials were provided.' }
      end
    else
--- db/migrate/20180722205726_create_site_logs.rb
+++ db/migrate/20180722205726_create_site_logs.rb
@@ -2,6 +2,7 @@ class CreateSiteLogs < ActiveRecord::Migration[5.1]
   def change
     create_table :site_logs do |t|
       t.text :module
+      t.text :submodule
       t.text :message
       t.references :site_log_type
       t.timestamps
