996ec3742a3155ae3126109b49543eab19be8009
Dale Myszewski II <dale.myszewski@gmail.com>
2018-11-22 21:15:06 -0600

    Adjusted Kaiden message

--- app/workers/kaiden_event_announcer_worker.rb
+++ app/workers/kaiden_event_announcer_worker.rb
@@ -6,7 +6,7 @@ class KaidenEventAnnouncerWorker
   def perform(*args)
     Event.where(published: true, published_discord: false).each do |event|
       discordUri = ENV["WEBHOOK_LINK"]
-      content = "@everyone A new operation has been posted to the Employee Portal! You can get more information here: https://my.bendrocorp.com/events/#{event.id}"
+      content = "@everyone An operation and it full details have been posted to the Employee Portal! You can get more information here: https://my.bendrocorp.com/events/#{event.id}"
       result = HTTParty.post(discordUri,
         :body => { :content => content}.to_json, :headers => { 'Content-Type' => 'application/json' } )
       if result.code == 204
