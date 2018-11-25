e79540902878498b0dfa44341e9715354d6917eb
Dale Myszewski II <dale.myszewski@gmail.com>
2018-11-24 23:01:33 -0600

    Fix for system map images to make all references optional

--- app/models/system_map_image.rb
+++ app/models/system_map_image.rb
@@ -1,11 +1,11 @@
 class SystemMapImage < ApplicationRecord
-  belongs_to :of_system, :class_name => 'SystemMapSystem', :foreign_key => 'of_system_id'
-  belongs_to :of_planet, :class_name => 'SystemMapSystemPlanetaryBody', :foreign_key => 'of_planet_id'
-  belongs_to :of_moon, :class_name => 'SystemMapSystemPlanetaryBodyMoon', :foreign_key => 'of_moon_id'
-  belongs_to :of_gravity_well, :class_name => 'SystemMapSystemGravityWellType', :foreign_key => 'of_gravity_well_id'
-  belongs_to :of_system_object, :class_name => 'SystemMapSystemObject', :foreign_key => 'of_system_object_id'
-  belongs_to :of_location, :class_name => 'SystemMapSystemPlanetaryBodyLocation', :foreign_key => 'of_location_id'
-  belongs_to :of_settlement, :class_name => 'SystemMapSystemSettlement', :foreign_key => 'of_settlement_id'
+  belongs_to :of_system, :class_name => 'SystemMapSystem', :foreign_key => 'of_system_id', optional: true
+  belongs_to :of_planet, :class_name => 'SystemMapSystemPlanetaryBody', :foreign_key => 'of_planet_id', optional: true
+  belongs_to :of_moon, :class_name => 'SystemMapSystemPlanetaryBodyMoon', :foreign_key => 'of_moon_id', optional: true
+  belongs_to :of_gravity_well, :class_name => 'SystemMapSystemGravityWellType', :foreign_key => 'of_gravity_well_id', optional: true
+  belongs_to :of_system_object, :class_name => 'SystemMapSystemObject', :foreign_key => 'of_system_object_id', optional: true
+  belongs_to :of_location, :class_name => 'SystemMapSystemPlanetaryBodyLocation', :foreign_key => 'of_location_id', optional: true
+  belongs_to :of_settlement, :class_name => 'SystemMapSystemSettlement', :foreign_key => 'of_settlement_id', optional: true
 
   has_attached_file :image, :styles => { :mini => "25x25#", :small => "50x50#", :thumbnail => "100x100#", :big => "200x200#", :original => "1920x1080>" },
                     #content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] },
