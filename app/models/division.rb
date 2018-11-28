a5a1c7bece8e1b6b191c698e8e5f3d535ef64f09
Dale Myszewski II <dale.myszewski@gmail.com>
2018-11-27 21:02:15 -0600

    Added RSI handle to by divisions profiles fetch

--- app/models/division.rb
+++ app/models/division.rb
@@ -8,7 +8,7 @@ class Division < ActiveRecord::Base
     division_member_arr = []
     self.characters.each do |character|
       if character.current_division == self
-        c = { id: character.id, avatar_url: character.avatar_url, full_name: character.full_name, first_name: character.first_name, last_name: character.last_name, current_job: character.current_job, current_job_level: character.current_job_level, division: character.current_division }
+        c = { id: character.id, avatar_url: character.avatar_url, full_name: character.full_name, first_name: character.first_name, last_name: character.last_name, current_job: character.current_job, current_job_level: character.current_job_level, division: character.current_division, rsi_handle: character.rsi_handle }
         division_member_arr << c
       end
     end
