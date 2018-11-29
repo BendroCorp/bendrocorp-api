bf25a9b51ce9e7fe35fedeabd1061dfa7372f78a
Dale Myszewski II <dale.myszewski@gmail.com>
2018-11-28 23:10:35 -0600

    Minor logging verbage fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -55,7 +55,7 @@ class SessionsController < ApplicationController
          render status: 403, json: { message: 'User not found or incorrect credentials were provided.' }
        end
      else
-       SiteLog.create(module: 'Session', submodule: 'User does not exist', message: "User could not authenticated. User does not exist! #{params[:session][:email]}", site_log_type_id: 1)
+       SiteLog.create(module: 'Session', submodule: 'User does not exist', message: "User could not authenticated. User does not exist! Used: #{params[:session][:email]}", site_log_type_id: 1)
        render status: 403, json: { message: 'User not found or incorrect credentials were provided.' }
      end
    else
