71bce22077d96d371c58f9fda38400d59d12e9cb
Dale Myszewski II <dale.myszewski@gmail.com>
2018-12-06 14:47:48 -0600

    Added token cleanup worker

---
+++ app/workers/token_cleanup_worker.rb
@@ -0,0 +1,10 @@
+class TokenCleanupWorker
+  include Sidekiq::Worker
+
+  def perform(*args)
+    @tokens = UserToken.where('expires < ?', Time.now).count
+    puts "#{UserToken.all.count} total user token(s). #{@tokens.count} expired tokens - cleaning up #{@tokens.count}"
+    @tokens.destroy_all
+    puts "#{UserToken.all.count} token(s) after cleanup"
+  end
+end
