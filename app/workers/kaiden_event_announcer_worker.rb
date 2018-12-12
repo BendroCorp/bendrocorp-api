4e3d487d072475646ed56fe591d5b947c2df6481
Dale Myszewski II <dale.myszewski@gmail.com>
2018-12-12 14:22:44 -0600

    Duplicated event announcements to spectrum-news channel. Resolves #92

--- app/workers/kaiden_event_announcer_worker.rb
+++ app/workers/kaiden_event_announcer_worker.rb
@@ -6,11 +6,20 @@ class KaidenEventAnnouncerWorker
 
   def perform(*args)
     Event.where(published: true, published_discord: false).each do |event|
-      discordUri = ENV["WEBHOOK_LINK"]
+      discordUri = ENV["WEBHOOK_LINK"] # general
+      discordNewsUri = ENV["NEWS_WEBHOOK_LINK"] # spectrum-news
+
+      # content to post
       content = "@everyone An operation and its full details have been posted to the Employee Portal! You can get more information here: https://my.bendrocorp.com/events/#{event.id}"
+
+      # send to both hooks
       result = HTTParty.post(discordUri,
         :body => { :content => content}.to_json, :headers => { 'Content-Type' => 'application/json' } )
-      if result.code == 204
+
+      # send to both hooks
+      result_two = HTTParty.post(discordNewsUri,
+        :body => { :content => content}.to_json, :headers => { 'Content-Type' => 'application/json' } )
+      if result.code == 204 && result_two.code == 204
         event.published_discord = true
         event.save!
       end
