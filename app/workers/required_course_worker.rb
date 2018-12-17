4d8623ac18d65bf28702610cc0ad3b5eb0eb7603
Dale Myszewski II <dale.myszewski@gmail.com>
2018-12-17 00:17:07 -0600

    Fix for required course worker

--- app/workers/required_course_worker.rb
+++ app/workers/required_course_worker.rb
@@ -4,21 +4,27 @@ class RequiredCourseWorker
   include Sidekiq::Worker
 
   def perform(*args)
-    User.all.each do |user|
-      if user.is_member?
-        todos = []
-        # TODO: Support division specific course requirements
-        TrainingCourse.where(archived: false, required: true).each do |course|
-          todo << course if course.training_course_completions.where(user: user, version: course.version).count == 0
-        end
+    required_courses = TrainingCourse.where(archived: false, required: true)
+    if required_courses.count > 0
+      User.all.each do |user|
+        if user.is_member?
+          todos = []
+          # TODO: Support division specific course requirements
 
-        mailTodoString = "<p>"
-        todos.each do |todo|
-          mailTodoString = "#{mailTodoString}</br><a href='https://my.bendrocorp.com/training/#{todo.id}'>#{todo.title}</a>"
-        end
-        mailTodoString = "#{mailTodoString}</p>"
+          required_courses.each do |course|
+            todos << course if course.training_course_completions.where(user: user, version: course.version).count == 0
+          end
 
-        EmailWorker.perform_async user.email, "Missing Required Courses", "<p>Hello #{user.main_character.full_name}</p><p>You currently have outstanding required courses:</p>#{mailTodoString}"
+          if todos.count > 0
+            mailTodoString = "<p>"
+            todos.each do |todo|
+              mailTodoString = "#{mailTodoString}</br><a href='https://my.bendrocorp.com/training/#{todo.id}'>#{todo.title}</a>"
+            end
+            mailTodoString = "#{mailTodoString}</p>"
+
+            EmailWorker.perform_async user.email, "Missing Required Courses", "<p>Hello #{user.main_character.full_name}</p><p>You currently have outstanding required courses:</p>#{mailTodoString}"
+          end
+        end
       end
     end
   end
