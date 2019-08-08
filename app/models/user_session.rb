6d46ec9fc06c255aee3a643fdf5fc2eefd30a53b
Dale Myszewski II <dale.myszewski@gmail.com>
2019-08-07 19:29:47 -0500

    More tweaks to user session

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -2,7 +2,15 @@ class UserSession < ApplicationRecord
     before_create { self.id = SecureRandom.uuid if self.id == nil && ENV["RAILS_ENV"] != 'production' }
 
     belongs_to :user
-    geocoded_by :ip_address,
-    :latitude => :latitude, :longitude => :longitude, :address => :location
-    after_validation :geocode
+    # geocoded_by :ip_address,
+    # :latitude => :latitude, :longitude => :longitude, :address => :location
+    geocoded_by :ip_address do |obj,results|
+        if (geo = results.first)
+            obj.latitude = geo.latitude
+            obj.longitude = geo.longitude
+            obj.location = geo.address
+            obj.country_code = geo.country_code
+        end
+    end
+after_validation :geocode
 end
