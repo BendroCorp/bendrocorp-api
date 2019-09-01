b5ec71d53cd3d7d039f3ba64c443056fafb2a3f3
Dale Myszewski II <dale.myszewski@gmail.com>
2019-08-31 20:21:16 -0500

    Removed offender name requirement

--- app/models/offender_report_offender.rb
+++ app/models/offender_report_offender.rb
@@ -2,7 +2,7 @@ class OffenderReportOffender < ApplicationRecord
   before_save { self.offender_name.downcase! }
 
   validates :offender_handle, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
-  validates :offender_name, presence: true, length: { maximum: 255 }
+  validates :offender_name, length: { maximum: 255 }
 
   has_many :offender_reports, -> { where report_approved: true, archived: false }, :class_name => 'OffenderReport', :foreign_key => 'offender_id'
   has_many :bounties, :class_name => 'OffenderBounty', :foreign_key => 'offender_id'
