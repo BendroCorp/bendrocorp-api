ea0eed764da525514faea468dfa1f4687ef042b4
Dale Myszewski II <dale.myszewski@gmail.com>
2020-01-11 14:56:09 -0600

    Added System Images and general code cleanup

--- app/controllers/system_map_moons_controller.rb
+++ app/controllers/system_map_moons_controller.rb
@@ -12,106 +12,17 @@ class SystemMapMoonsController < ApplicationController
     a.require_one_role([23]) # admin
   end
 
-  # def add_moon
-  #   @moon = SystemMapSystemPlanetaryBodyMoon.new()
-  #   # :title, :description, :orbits_planet_id
-  #   @moon.title = params[:moon][:title]
-  #   @moon.description = params[:moon][:description]
-  #   @moon.orbits_planet_id = params[:moon][:orbits_planet_id]
-  #   @moon.population_density = params[:moon][:population_density]
-  #   @moon.economic_rating = params[:moon][:economic_rating]
-  #   @moon.general_radiation = params[:moon][:general_radiation]
-  #   @moon.atmospheric_height = params[:moon][:atmospheric_height]
-  #   @moon.atmo_pressure = params[:moon][:atmo_pressure]
-  #   @moon.tempature_min = params[:moon][:tempature_min]
-  #   @moon.tempature_max = params[:moon][:tempature_max]
-  #   @moon.tempature_min = params[:moon][:tempature_min]
-  #   @moon.minimum_criminality_rating = params[:moon][:minimum_criminality_rating]
-  #   @moon.jurisdiction_id = params[:moon][:jurisdiction_id]
-
-  #   @moon.discovered_by_id = current_user.id
-
-  #   if params[:moon][:new_primary_image] != nil
-  #     if @moon.primary_image != nil
-  #       @moon.primary_image.image = params[:moon][:new_primary_image][:base64]
-  #       @moon.primary_image.image_file_name = params[:moon][:new_primary_image][:name]
-  #     else
-  #       @moon.primary_image = SystemMapImage.create(created_by_id: current_user.id, image: params[:moon][:new_primary_image][:base64], image_file_name: params[:moon][:new_primary_image][:name], title: @moon.title, description: @moon.title)
-  #     end
-  #   end
-  #   if @moon.save
-  #     render status: 201, json: @moon.as_json(include: { jurisdiction: { include: { categories: { include: { laws: {} } } } }, flora: {}, fauna: {}, settlements: { include: { jurisdiction: { include: { categories: { include: { laws: {} } } } }, locations: { methods: [:primary_image_url, :primary_image_url_full] } }, methods: [:primary_image_url, :primary_image_url_full] }, discovered_by: { only: [], methods: [:main_character] }, system_map_images: {}, locations: { methods: [:primary_image_url, :primary_image_url_full] }, moon_types: {}, system_objects: { include: { jurisdiction: { include: { categories: { include: { laws: {} } } } }, flora: {}, fauna: {}, object_type: {}, locations: { methods: [:primary_image_url, :primary_image_url_full] } }, methods: [:primary_image_url, :primary_image_url_full] }, atmo_compositions: { include: { atmo_gas: {} } } }, methods: [:primary_image_url, :primary_image_url_full])
-  #   else
-  #     render status: 500, json: { message: "Moon could not be created because: #{@moon.errors.full_messages.to_sentence}" }
-  #   end
-  # end
-
-  # # PATCH/PUT api/system-map/moon
-  # def update_moon
-  #   @moon = SystemMapSystemPlanetaryBodyMoon.find_by_id(params[:moon][:id].to_i)
-  #   if @moon != nil
-
-  #     @moon.title = params[:moon][:title]
-  #     @moon.description = params[:moon][:description]
-  #     @moon.population_density = params[:moon][:population_density]
-  #     @moon.economic_rating = params[:moon][:economic_rating]
-  #     @moon.general_radiation = params[:moon][:general_radiation]
-  #     @moon.atmospheric_height = params[:moon][:atmospheric_height]
-  #     @moon.atmo_pressure = params[:moon][:atmo_pressure]
-  #     @moon.tempature_min = params[:moon][:tempature_min]
-  #     @moon.tempature_max = params[:moon][:tempature_max]
-  #     @moon.minimum_criminality_rating = params[:moon][:minimum_criminality_rating]
-  #     @moon.jurisdiction_id = params[:moon][:jurisdiction_id]
-
-  #     if params[:moon][:new_primary_image] != nil
-  #       if @moon.primary_image != nil
-  #         @moon.primary_image.image = params[:moon][:new_primary_image][:base64]
-  #         @moon.primary_image.image_file_name = params[:moon][:new_primary_image][:name]
-  #       else
-  #         @moon.primary_image = SystemMapImage.create(created_by_id: current_user.id, image: params[:moon][:new_primary_image][:base64], image_file_name: params[:moon][:new_primary_image][:name], title: @moon.title, description: @moon.title)
-  #       end
-  #     end
-  #     if @moon.update_attributes(moon_params)
-  #       render status: 200, json: @moon.as_json(include: { jurisdiction: { include: { categories: { include: { laws: {} } } } }, flora: {}, fauna: {}, settlements: { include: { jurisdiction: { include: { categories: { include: { laws: {} } } } }, locations: { methods: [:primary_image_url, :primary_image_url_full] } }, methods: [:primary_image_url, :primary_image_url_full] }, discovered_by: { only: [], methods: [:main_character] }, system_map_images: {}, locations: { methods: [:primary_image_url, :primary_image_url_full] }, moon_types: {}, system_objects: { include: { jurisdiction: { include: { categories: { include: { laws: {} } } } }, flora: {}, fauna: {}, object_type: {}, locations: { methods: [:primary_image_url, :primary_image_url_full] } }, methods: [:primary_image_url, :primary_image_url_full] }, atmo_compositions: { include: { atmo_gas: {} } } }, methods: [:primary_image_url, :primary_image_url_full])
-  #     else
-  #       render status: 500, json: { message: "Moon could not be created because: #{@moon.errors.full_messages.to_sentence}"}
-  #     end
-  #   else
-  #     #
-  #     render status: 404, json: { message: "Moon not found. You lost a moon??" }
-  #   end
-  # end
-
-  # # DELETE api/system-map/moon/:moon_id
-  # def delete_moon
-  #   begin
-  #       @moon = SystemMapSystemPlanetaryBodyMoon.find_by_id(params[:moon_id].to_i)
-  #       if @moon != nil
-  #         @moon.archived = true
-  #         if @moon.save
-  #           render status: 200, json: { message: "Moon archived." }
-  #         else
-  #           render status: 500, json: { message: "Moon could not be archived because: #{@moon.errors.full_messages.to_sentence}"}
-  #         end
-  #       else
-  #         render status: 404, json: { message: "Moon not found." }
-  #       end
-  #   rescue => e
-  #     render status: 500, json: { message: "ERROR Occured: Moon could not be saved: " + e.message}
-  #   end
-  # end
-
   # GET /system_map_moons
   def index
     @system_map_moons = SystemMapSystemPlanetaryBodyMoon.where(archived: false)
 
-    render json: @system_map_moons.to_json(include: { settlements: {}, locations: { include: { mission_givers: {} }}, system_objects: {}, faction_affiliation: {}, jurisdiction: {} }, methods: [:kind, :primary_image_url, :parent, :title_with_kind])
+    render json: @system_map_moons.to_json(include: { system_map_images: {}, settlements: {}, locations: { include: { mission_givers: {} }}, system_objects: {}, faction_affiliation: {}, jurisdiction: {} }, methods: [:kind, :primary_image_url, :parent, :title_with_kind])
   end
 
   # GET /system_map_moons/1
   def show
     if @system_map_moon
-      render json: @system_map_moon.to_json(include: { settlements: {}, locations: { include: { mission_givers: {} }}, system_objects: {}, faction_affiliation: {}, jurisdiction: {} }, methods: [:kind, :primary_image_url, :parent, :title_with_kind])
+      render json: @system_map_moon.to_json(include: { system_map_images: {}, settlements: {}, locations: { include: { mission_givers: {} }}, system_objects: {}, faction_affiliation: {}, jurisdiction: {} }, methods: [:kind, :primary_image_url, :parent, :title_with_kind])
     else
       render status: 404, json: { message: 'Moon not found!' }
     end
@@ -131,7 +42,7 @@ class SystemMapMoonsController < ApplicationController
     end
 
     if @system_map_moon.save
-      render json: @system_map_moon.to_json(include: { settlements: {}, locations: { include: { mission_givers: {} }}, system_objects: {}, faction_affiliation: {}, jurisdiction: {} }, methods: [:kind, :primary_image_url, :parent, :title_with_kind]), status: :created
+      render json: @system_map_moon.to_json(include: { system_map_images: {}, settlements: {}, locations: { include: { mission_givers: {} }}, system_objects: {}, faction_affiliation: {}, jurisdiction: {} }, methods: [:kind, :primary_image_url, :parent, :title_with_kind]), status: :created
     else
       render json: { message: @system_map_moon.errors.full_messages.to_sentence }, status: :unprocessable_entity
     end
@@ -151,7 +62,7 @@ class SystemMapMoonsController < ApplicationController
       end
 
       if @system_map_moon.update(system_map_moon_params)
-        render json: @system_map_moon.to_json(include: { settlements: {}, locations: { include: { mission_givers: {} }}, system_objects: {}, faction_affiliation: {}, jurisdiction: {} }, methods: [:kind, :primary_image_url, :parent, :title_with_kind])
+        render json: @system_map_moon.to_json(include: { system_map_images: {}, settlements: {}, locations: { include: { mission_givers: {} }}, system_objects: {}, faction_affiliation: {}, jurisdiction: {} }, methods: [:kind, :primary_image_url, :parent, :title_with_kind])
       else
         render json: { message: @system_map_moon.errors.full_messages.to_sentence }, status: :unprocessable_entity
       end
--- app/controllers/system_map_settlements_controller.rb
+++ app/controllers/system_map_settlements_controller.rb
@@ -12,83 +12,17 @@ class SystemMapSettlementsController < ApplicationController
     a.require_one_role([23]) # admin
   end
 
-  # def add_settlement
-  #   @system_map_settlement = SystemMapSystemSettlement.new
-  #   if params[:settlement][:new_primary_image] != nil
-  #     if @system_map_settlement.primary_image != nil
-  #       @system_map_settlement.primary_image.image = params[:settlement][:new_primary_image][:base64]
-  #       @system_map_settlement.primary_image.image_file_name = params[:settlement][:new_primary_image][:name]
-  #     else
-  #       @system_map_settlement.primary_image = SystemMapImage.create(created_by_id: current_user.id, image: params[:settlement][:new_primary_image][:base64], image_file_name: params[:settlement][:new_primary_image][:name], title: @system_map_settlement.title, description: @system_map_settlement.title)
-  #     end
-  #   end
-  #   @system_map_settlement.title = params[:settlement][:title]
-  #   @system_map_settlement.description = params[:settlement][:description]
-  #   @system_map_settlement.on_planet_id = params[:settlement][:on_planet_id]
-  #   @system_map_settlement.on_moon_id = params[:settlement][:on_moon_id]
-  #   @system_map_settlement.jurisdiction_id = params[:settlement][:jurisdiction_id]
-
-  #   @system_map_settlement.discovered_by_id = current_user.id
-
-  #   if @system_map_settlement.save
-  #     render status: 200, json: @system_map_settlement.as_json(include: { jurisdiction: { include: { categories: { include: { laws: {} } } } }, locations: { methods: [:primary_image_url, :primary_image_url_full] } }, methods: [:primary_image_url, :primary_image_url_full] )
-  #   else
-  #     render status: 500, json: { message: "ERROR Occured: New settlement could not be saved."}
-  #   end
-  # end
-
-  # def update_settlement
-  #   @system_map_settlement = SystemMapSystemSettlement.find_by_id(params[:settlement][:id].to_i)
-  #   if @system_map_settlement != nil
-  #     if params[:settlement][:new_primary_image] != nil
-  #       if @system_map_settlement.primary_image != nil
-  #         @system_map_settlement.primary_image.image = params[:settlement][:new_primary_image][:base64]
-  #         @system_map_settlement.primary_image.image_file_name = params[:settlement][:new_primary_image][:name]
-  #       else
-  #         new_primary_image = SystemMapImage.create(created_by_id: current_user.id, image: params[:settlement][:new_primary_image][:base64], image_file_name: params[:settlement][:new_primary_image][:name])
-  #         @system_map_settlement.primary_image = new_primary_image
-  #       end
-  #     end
-  #     @system_map_settlement.title = params[:settlement][:title]
-  #     @system_map_settlement.description = params[:settlement][:description]
-  #     @system_map_settlement.on_planet_id = params[:settlement][:on_planet_id]
-  #     @system_map_settlement.on_moon_id = params[:settlement][:on_moon_id]
-  #     @system_map_settlement.jurisdiction_id = params[:settlement][:jurisdiction_id]
-
-  #     if @system_map_settlement.save
-  #       render status: 200, json: @system_map_settlement.as_json(include: { jurisdiction: { include: { categories: { include: { laws: {} } } } }, locations: { methods: [:primary_image_url, :primary_image_url_full] } }, methods: [:primary_image_url, :primary_image_url_full] )
-  #     else
-  #       render status: 500, json: { message: "ERROR Occured: New system could not be saved: " + e.message}
-  #     end
-  #   else
-  #     render status: 404, json: { message: "Settlement not found." }
-  #   end
-  # end
-
-  # def delete_settlement
-  #   @system_map_settlement = SystemMapSystemSettlement.find_by_id(params[:settlement_id].to_i)
-  #   if @system_map_settlement != nil
-  #     if @system_map_settlement.destroy
-  #       render status: 200, json: { message: "Success" }
-  #     else
-  #       render status: 500, json: { message: "ERROR Occured: New system could not be saved: " + e.message}
-  #     end
-  #   else
-  #     render status: 404, json: { message: "Settlement not found." }
-  #   end
-  # end  
-
   # GET /system_map_settlements
   def index
     @system_map_settlements = SystemMapSystemSettlement.where(archived: false)
 
-    render json: @system_map_settlements.to_json(include: { locations: { include: { mission_givers: {} } }, faction_affiliation: {}, jurisdiction: {} }, methods: [:kind, :primary_image_url, :parent, :title_with_kind])
+    render json: @system_map_settlements.to_json(include: { system_map_images: {}, locations: { include: { mission_givers: {} } }, faction_affiliation: {}, jurisdiction: {} }, methods: [:kind, :primary_image_url, :parent, :title_with_kind])
   end
 
   # GET /system_map_settlements/1
   def show
     if @system_map_settlement
-      render json: @system_map_settlement.to_json(include: { locations: { include: { mission_givers: {} } }, faction_affiliation: {}, jurisdiction: {} }, methods: [:kind, :primary_image_url, :parent, :title_with_kind])
+      render json: @system_map_settlement.to_json(include: { system_map_images: {}, locations: { include: { mission_givers: {} } }, faction_affiliation: {}, jurisdiction: {} }, methods: [:kind, :primary_image_url, :parent, :title_with_kind])
     else
       render status: 404, json: { message: 'Settlement not found!' }
     end
@@ -111,7 +45,7 @@ class SystemMapSettlementsController < ApplicationController
     end
 
     if @system_map_settlement.save
-      render json: @system_map_settlement.to_json(include: { locations: { include: { mission_givers: {} } }, faction_affiliation: {}, jurisdiction: {} }, methods: [:kind, :primary_image_url, :parent, :title_with_kind]), status: :created
+      render json: @system_map_settlement.to_json(include: { system_map_images: {}, locations: { include: { mission_givers: {} } }, faction_affiliation: {}, jurisdiction: {} }, methods: [:kind, :primary_image_url, :parent, :title_with_kind]), status: :created
     else
       render json: { message: @system_map_settlement.errors.full_messages.to_sentence }, status: :unprocessable_entity
     end
@@ -131,7 +65,7 @@ class SystemMapSettlementsController < ApplicationController
       end
 
       if @system_map_settlement.update(system_map_settlement_params)
-        render json: @system_map_settlement.to_json(include: { locations: { include: { mission_givers: {} } }, faction_affiliation: {}, jurisdiction: {} }, methods: [:kind, :primary_image_url, :parent, :title_with_kind])
+        render json: @system_map_settlement.to_json(include: { system_map_images: {}, locations: { include: { mission_givers: {} } }, faction_affiliation: {}, jurisdiction: {} }, methods: [:kind, :primary_image_url, :parent, :title_with_kind])
       else
         render json: { message: @system_map_settlement.errors.full_messages.to_sentence }, status: :unprocessable_entity
       end
