6588b4b112be48f436ab8fbfc894d6ef42aa570a
Dale Myszewski <dale.myszewski@gmail.com>
2020-02-28 23:51:21 -0600

    Completed #339

--- app/models/event_certification_request.rb
+++ app/models/event_certification_request.rb
@@ -47,34 +47,6 @@ class EventCertificationRequest < ApplicationRecord
     else
       raise 'Error occurred during event approval completion. All attendences could not be certified.'
     end
-
-    # check to see if this event is suppose to recur
-    if self.event.weekly_recurrence || self.event.monthly_recurrence
-      @event = Event.new
-      @event.name = self.event.name
-      @event.description = self.event.description
-      @event.weekly_recurrence = self.event.weekly_recurrence
-      @event.monthly_recurrence = self.event.monthly_recurrence
-      @event.event_type_id = self.event.event_type_id
-      @event.livestream_url = self.event.livestream_url
-
-      @event.start_date = self.event.start_date + 7.days if self.event.weekly_recurrence
-      @event.end_date = self.event.end_date + 7.days if self.event.weekly_recurrence
-
-      @event.start_date = self.event.start_date + 4.weeks if self.event.monthly_recurrence
-      @event.end_date = self.event.end_date + 4.weeks if self.event.monthly_recurrence
-
-      @event.briefing = EventBriefing.new
-      @event.debriefing = EventDebriefing.new
-
-      self.event.event_awards.each do |event_award|
-        @event.event_awards << EventAward.new(award_id: event_award.award_id)
-      end
-
-      if !@event.save
-        throw "ERROR Occured: Reccurring event could not be saved."
-      end
-    end
   end
 
   def approval_denied
