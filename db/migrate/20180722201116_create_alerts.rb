6c26fe2cf23de602beebe4fd2878eb48acba8818
Dale Myszewski <dale.myszewski@gmail.com>
2021-02-01 10:53:22 -0600

    Progress. rebase

--- db/migrate/20180722201116_create_alerts.rb
+++ db/migrate/20180722201116_create_alerts.rb
@@ -6,12 +6,12 @@ class CreateAlerts < ActiveRecord::Migration[5.1]
       t.datetime :expires
       t.boolean :archived
 
-      t.belongs_to :system
-      t.belongs_to :planet
-      t.belongs_to :moon
-      t.belongs_to :system_object
-      t.belongs_to :settlement
-      t.belongs_to :location
+      # t.belongs_to :system
+      # t.belongs_to :planet
+      # t.belongs_to :moon
+      # t.belongs_to :system_object
+      # t.belongs_to :settlement
+      # t.belongs_to :location
       t.belongs_to :alert_type
       t.belongs_to :issued_by
       t.timestamps
