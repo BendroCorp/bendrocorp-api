1e99d889feb063e527464a1d48e57fd99a242adc
Dale Myszewski II <dale.myszewski@gmail.com>
2021-02-04 13:12:35 -0600

    Added title_with_kind fetch in alerts

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -9,13 +9,13 @@ class AlertsController < ApplicationController
 
   # GET api/alert
   def list
-    render status: 200, json: Alert.where("archived = ? AND (expires > ? OR expires IS NULL)", false, Time.now).order('created_at desc').as_json(include: { star_object: { methods: [:kind, :primary_image_url] }, alert_type: {}, user: { only:[:id], methods: [:main_character] } })
+    render status: 200, json: Alert.where("archived = ? AND (expires > ? OR expires IS NULL)", false, Time.now).order('created_at desc').as_json(include: { star_object: { methods: [:kind, :title_with_kind, :primary_image_url] }, alert_type: {}, user: { only:[:id], methods: [:main_character] } })
   end
 
   # GET api/alert/:id
   def show
     if !@alert.nil?
-      render status: 200, json: @alert.as_json(include: { star_object: { methods: [:kind, :primary_image_url] }, alert_type: {}, user: { only:[:id], methods: [:main_character] } })
+      render status: 200, json: @alert.as_json(include: { star_object: { methods: [:kind, :title_with_kind, :primary_image_url] }, alert_type: {}, user: { only:[:id], methods: [:main_character] } })
     else
       render status: 404, json: { message: 'Alert not found.' }
     end
@@ -27,7 +27,7 @@ class AlertsController < ApplicationController
     @alert.user_id = current_user.id
     @alert.expires = Time.at(params[:alert][:expires_ms] / 1000.0) if params[:alert][:expires_ms]
     if @alert.save
-      render status: 201, json: @alert.as_json(include: { star_object: { methods: [:kind, :primary_image_url] }, alert_type: {}, user: { only:[:id], methods: [:main_character] } })
+      render status: 201, json: @alert.as_json(include: { star_object: { methods: [:kind, :title_with_kind, :primary_image_url] }, alert_type: {}, user: { only:[:id], methods: [:main_character] } })
     else
       render status: 500, json: { message: "Alert could not be created because: #{@alert.errors.full_messages.to_sentence}"}
     end
@@ -38,7 +38,7 @@ class AlertsController < ApplicationController
     if !@alert.nil?
       @alert.expires = Time.at(params[:alert][:expires_ms] / 1000.0) if params[:alert][:expires_ms]
       if @alert.update_attributes(alert_params)
-        render status: 200, json: @alert.as_json(include: { star_object: { methods: [:kind, :primary_image_url] }, alert_type: {}, user: { only:[:id], methods: [:main_character] } })
+        render status: 200, json: @alert.as_json(include: { star_object: { methods: [:kind, :title_with_kind, :primary_image_url] }, alert_type: {}, user: { only:[:id], methods: [:main_character] } })
       else
         render status: 500, json: { message: "Alert could not be created because: #{@alert.errors.full_messages.to_sentence}"}
       end
