f254d907323c4563bee489e5558bd3367ba0beec
Dale Myszewski <dale.myszewski@gmail.com>
2021-06-10 23:24:35 -0500

    Final files mig

--- db/migrate/20210612222640_migrate_paperclip_to_active_storage.rb
+++ db/migrate/20210612222640_migrate_paperclip_to_active_storage.rb
@@ -9,7 +9,7 @@ class MigratePaperclipToActiveStorage < ActiveRecord::Migration[6.1]
       # uri = 'https://decider.com/wp-content/uploads/2020/12/the-mandalorian-14-boba-fett-headshot.jpg?quality=80&strip=all&w=1200'
       puts uri
       # not the right URI: /system/characters/avatars/000/000/001/original/rindzer.png >://
-      next if uri.nil? || uri.include?("missing") #|| character.avatar.attached?
+      next if uri.nil? || uri.include?("missing") || character.avatar.attached?
       # https://github.com/janko/image_processing/blob/master/doc/minimagick.md#resize_to_fill
       # actually make the move
       icount += 1
@@ -34,7 +34,7 @@ class MigratePaperclipToActiveStorage < ActiveRecord::Migration[6.1]
     SystemMapImage.all.each do |image|
       uri = image.paperclip_original_uri
 
-      next if uri.nil? || uri.include?("missing") #|| image.image.attached?
+      next if uri.nil? || uri.include?("missing") || image.image.attached?
 
       # create and perform a base resize on the image
       icount += 1
@@ -60,7 +60,7 @@ class MigratePaperclipToActiveStorage < ActiveRecord::Migration[6.1]
     ImageUpload.all.each do |image|
       uri = image.paperclip_original_uri
 
-      next if uri.nil? || uri.include?("missing") #|| image.image.attached?
+      next if uri.nil? || uri.include?("missing") || image.image.attached?
 
       # create and perform a base resize on the image
       icount += 1
