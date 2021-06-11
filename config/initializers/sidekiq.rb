1910d99057eb38cb5fc540630f5240303935ab03
Dale Myszewski <dale.myszewski@gmail.com>
2021-06-10 23:38:33 -0500

    sidekiq fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,13 @@
 include Error::WorkerErrorHandler
 
-puts "Sidekiq env #{Rails.env.to_sym}"
+puts "Sidekiq env #{RENV["RAILS_ENV"].to_sym}"
 
 Sidekiq.default_worker_options = { 'backtrace' => true }
 Sidekiq.configure_server do |config|
   config.error_handlers << Proc.new {|ex,ctx_hash| Error::WorkerErrorHandler.handle(ex, ctx_hash) }
-  config.redis = { url: (Rails.application.credentials[Rails.env.to_sym][:redis_url] || 'redis://localhost:6379/1') }
+  config.redis = { url: (Rails.application.credentials[ENV["RAILS_ENV"].to_sym][:redis_url] || 'redis://localhost:6379/1') }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: (Rails.application.credentials[Rails.env.to_sym][:redis_url] || 'redis://localhost:6379/1') }
+  config.redis = { url: (Rails.application.credentials[ENV["RAILS_ENV"].to_sym][:redis_url] || 'redis://localhost:6379/1') }
 end
