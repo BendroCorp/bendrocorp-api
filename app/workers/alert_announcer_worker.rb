163b30a75d203a6fecab0d45fa3ca359f7fab693
Dale Myszewski II <dale.myszewski@gmail.com>
2021-07-14 12:25:14 -0500

    alert announcer fix

--- app/workers/alert_announcer_worker.rb
+++ app/workers/alert_announcer_worker.rb
@@ -2,24 +2,24 @@ class AlertAnnouncerWorker
   include Sidekiq::Worker
 
   def perform()
-    Alert.where(published: false, approved: true, archived: false).each do |alert|
+    Alert.where(published: false, approved: true, archived: false).each do |alert_item|
       # set push message
       push_message =
-        if alert.alert_type_id == 'b750318c-57ae-4801-bbef-9848c6e02880' # is CSAR
-          "CSAR: #{alert.user.main_character.full_name} is requesting rescue from #{alert.star_object.title}!"
+        if alert_item.alert_type_id == 'b750318c-57ae-4801-bbef-9848c6e02880' && !alert_item.user.nil? # is CSAR
+          "CSAR: #{alert_item.user.main_character.full_name} is requesting rescue from #{alert_item.star_object.title}!"
         else
-          "A new #{alert.alert_type} has been posted!"
+          "A new #{alert_item.alert_type} has been posted!"
         end
 
       PushWorker.perform_async(
         user.id,
         push_message,
         'ALERT_NOTICE',
-        { alert: alert.id }
+        { alert: alert_item.id }
       )
 
-      alert.published = true
-      alert.save!
+      alert_item.published = true
+      alert_item.save
     end
   end
 end
